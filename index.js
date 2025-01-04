5a456c07f1eb8af61652b9282122d8c4114bcdec
dmm341 <muriithidaniel977@gmail.com>
2025-01-04 22:54:36 +0300

    objectmethod

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
  
  let name='dan';
  console.log(name);
-let person ={}
-//guyvgyuviyu
+let person ={
+    Name:'dan',
+    second:'maina',
+    age:'30',
+    gender:'male'
+}
+person.location='nakuru'
+console.log(person);
+document.getElementById("demo").innerHTML=person.Name + ' ' + person.age;
+person.fullname = function(){
+    return this.Name+'   '+this.second ;
+};
+document.getElementById("nam").innerHTML=person.fullname()
